dd0d83c04f5ab3747eb7129a34acee1edaf36a34
kyleahern-png <kyleahern@utexas.edu>
2024-12-09 23:34:35 +0000

    Add working changes for The Poptomist

--- gulpfile.js
+++ gulpfile.js
@@ -13,20 +13,35 @@ import scripts from './tasks/scripts.js';
 import serve from './tasks/serve.js';
 import styles from './tasks/styles.js';
 
-// default tasks
 gulp.task('default', (done) => {
-  runSequence(
-    [clean, styles, copy, gulp.parallel(scripts, images), nunjucks, bake],
-    lint,
-    format,
-    done
-  );
+  try {
+    runSequence(
+      [clean, styles, copy, gulp.parallel(scripts, images), nunjucks, bake],
+      lint,
+      format,
+      done
+    );
+  } catch (err) {
+    console.error('Error in default task:', err);
+    done(err);
+  }
 });
 
-// run default tasks and then serve locally
-gulp.task('dev', gulp.series('default', serve));
+gulp.task('dev', gulp.series('default', (done) => {
+  try {
+    serve();
+    done();
+  } catch (err) {
+    console.error('Error starting dev server:', err);
+    done(err);
+  }
+}));
 
-// Allow them to be called individually
 gulp.task(clear);
 gulp.task(fetch);
 gulp.task(format);
+
+gulp.task('debug', gulp.series(clean, copy, nunjucks, bake, (done) => {
+  console.log('Debug tasks completed successfully');
+  done();
+}));
